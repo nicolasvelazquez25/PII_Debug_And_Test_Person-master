2d31f264f30c5622eb2a77f9aaf9965f98de7dff
nicolasvelazquez25 <nicolasvelazquezperez@gmail.com>
2020-09-12 21:48:37 -0300

    Actualizo los test ya funcionales

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,10 +6,10 @@ namespace UnitTestAndDebug
     {
         static void Main(string[] args)
         {
-            Person john = new Person("John Doe", "1.234.567-8");
-            Person jane = new Person("Jane Doe", "8.765.432-1");
-            john.IntroduceYourself();
-            jane.IntroduceYourself();
+            //Person john = new Person("John Doe", "1.234.567-2");
+            //Person jane = new Person("Jane Doe", "8.765.432-1");
+            //john.IntroduceYourself();
+            //jane.IntroduceYourself();
         }
     }
 }
--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -4,6 +4,8 @@ using UnitTestAndDebug;
 
 using System.Linq;
 
+using System;
+
 namespace Tests
 {
     public class Tests
@@ -11,97 +13,46 @@ namespace Tests
 
         private Person persona;
 
-        private Person persona_nombre_vacio;
-
-        private Person persona_cedula_vacio;
-
-        private Person persona_vacio;
-
-        private Person persona_nombre_null;
-
-        private Person persona_cedula_null;
-
-        private Person persona_null;
-
         [SetUp]
         public void Setup()
         {
-            persona = new Person("John Doe", "1.234.567-8");
-            persona_nombre_vacio = new Person("", "1.234.567-8");
-            persona_cedula_vacio = new Person("John Doe", "");
-            persona_vacio = new Person("", "");
-            persona_nombre_null = new Person(null, "1.234.567-8");
-            persona_cedula_null = new Person("John Doe", null);
-            persona_null = new Person(null, null);
+            persona = new Person("John Doe", "1.234.567-2");
+        }
+        [Test]
+        public void Test_Persona()
+        {
+            persona = new Person("John Doe", "1.234.567-2");
+            Assert.IsNotNull(persona.Name);
+            Assert.IsNotNull(persona.ID);
         }
 
         [Test]
-        public void Test_Isvalid_True()
-        {
-            bool result = IdUtils.IdIsValid("1.234.567-8");
-            Assert.AreEqual(true, result);
-        }
-
-        public void Test_Isvalid_Sin_Puntos()
-        {
-            bool result = IdUtils.IdIsValid("12345678");
-            Assert.AreEqual(true, result);
-        }
-
-        public void Test_Isvalid_Exceso()
-        {
-            bool result = IdUtils.IdIsValid("1.234.567-80");
-            Assert.AreEqual(false, result);
-        }
-        public void Test_Isvalid_Falta()
-        {
-            bool result = IdUtils.IdIsValid("1.234.567-");
-            Assert.AreEqual(false, result);
-        }
-        public void Test_Isvalid_Vacio()
-        {
-            bool result = IdUtils.IdIsValid("");
-            Assert.AreEqual(false, result);
-        }
-        public void Test_Isvalid_Null()
-        {
-            bool result = IdUtils.IdIsValid(null);
-            Assert.AreEqual(false, result);
-        }
-        public void Test_Persona()
-        {
-            Person result = new Person("John Doe", "1.234.567-8");
-            Assert.AreEqual(persona, result);
-        }
         public void Test_Persona_Nombre_Vacio()
         {
-            Person result = new Person("", "1.234.567-8");
-            Assert.AreEqual(persona_nombre_vacio, result);
+            persona = new Person("", "1.234.567-2");
+            Assert.IsNull(persona.Name);
         }
+
+        [Test]
         public void Test_Persona_Cedula_Vacio()
         {
-            Person result = new Person("John Doe", "");
-            Assert.AreEqual(persona_cedula_vacio, result);
+            persona = new Person("John Doe", "");
+            Assert.IsNull(persona.ID);
         }
+
+        [Test]
         public void Test_Persona_Vacio()
         {
-            Person result = new Person("", "");
-            Assert.AreEqual(persona_vacio, result);
+            persona = new Person("", "");
+            Assert.IsNull(persona.Name);
+            Assert.IsNull(persona.ID);
         }
+
+        [Test]
         public void Test_Persona_Nombre_Null()
         {
-            Person result = new Person(null, "1.234.567-8");
-            Assert.AreEqual(persona_nombre_null, result);
-        }
-        public void Test_Persona_Cedula_Null()
-        {
-            Person result = new Person("John Doe", null);
-            Assert.AreEqual(persona_cedula_null, result);
-        }
-        public void Test_Persona_Null()
-        {
-            Person result = new Person(null, null);
-            Assert.AreEqual(persona_null, result);
-        }
+            persona = new Person(null, "1.234.567-2");
+            Assert.IsNull(persona.Name);
+        }       
     }
 }
